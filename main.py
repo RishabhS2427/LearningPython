19f5bac8219e97bf7a5a75418ae6573ad00a0187
RishabhS2427 <rishabhshri2795@gmail.com>
2026-03-19 20:48:44 +0530

    function practice

--- main.py
+++ main.py
@@ -3,14 +3,37 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+class Graph():
+    def __init__(self, id):
+        self.id = id
+        self.id = 80
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    val = Graph(200)
+    print(val.id)
 
 
+def add (a, b) :
+    return (a + b)
+
+
+def add3 (a, b) :
+    c = a + b
+    return (c)
+
+def add2(a,b) :
+    return sum(a + b)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(add(12, 13))
+    print(add3(14, 13))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# val = Graph(200)
+# print(val.id)
+
