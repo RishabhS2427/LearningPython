46460211fb15d6c85f37d19beb20f69dd756522d
RishabhS2427 <rishabhshri2795@gmail.com>
2026-03-29 05:09:02 +0530

    fundamentals about rest api and a quick demo to fetch data of bitcoin and plot it

--- pandas_impl/pandas_demo.py
+++ pandas_impl/pandas_demo.py
@@ -1,24 +1,20 @@
 import pandas as pd
 
+def createDict():
+    dict_ = {'a' : [11,12,13], 'b' : [21,22,23]}
+    df = pd.DataFrame(dict_)
+    print("------------------------")
+    print(df.head())
+    # calculate mean
+    print("------------------------")
+    print(df.mean())
+    #discription all about the df
+    print("------------------------")
+    print(df.describe())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def main():
+    createDict()
 
 if __name__ == '__main__':
-    print("Pandas demo full integration")
+    print("Pandas demo")
+    createDict()
